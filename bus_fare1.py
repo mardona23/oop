7be17fc8eca1415427fb70d62afc2e4b7415ce8f
mardona23 <adam_23@outlook.dk>
2025-07-10 11:38:55 +0200

    bus

--- bus_fare1.py
+++ bus_fare1.py
@@ -6,4 +6,12 @@ class vehicle:
         print(self.name)
         print(self.number_of_seats)
 class bus(vehicle):
-    def __init__(self, number_of_seats, name):
+    def __init__(self, number_of_seats, name,distance ):
+        self.distance=distance
+    def __init__(self,number_of_seats,name):     
+    def fare(self,distance):
+        print(distance*126)
+    vehicle.. __init__(self,number_of_seats,name)
+obg1=bus(23,"mercedes")
+obg1.intro()
+obg1.fare(2)
